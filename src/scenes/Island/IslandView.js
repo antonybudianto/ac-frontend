025f0f2fe0a5d8e075fe2549a4f93af75bbae0aa
Antony <antony.budianto@tokopedia.com>
2020-07-28 22:24:08 +0700

    init new carousel

--- src/scenes/Island/IslandView.js
+++ src/scenes/Island/IslandView.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
 function IslandView({ match }) {
   const [island, setIsland] = useState(null);
 
@@ -21,46 +24,33 @@ function IslandView({ match }) {
     <div className="container container-mobile">
       {island ? (
         <>
-          <div
-            className="jumbotron text-center"
-            style={{
-              height: '280px',
-              backgroundSize: 'cover',
-              backgroundImage: `url(${island.cover})`,
-            }}
-          ></div>
-          <div className="col-md-12 mb-5">
+          <div className="col-md-12 mb-5 mt-5">
             <h1>{island.name}</h1>
             <div>{island.fruit} fruit</div>
           </div>
           <div className="col-md-12 mb-5">
-            <h3>Gallery</h3>
             <div className="gallery-container">
-              {(island.gallery || [0, 0, 0, 0, 0, 0]).map((g, i) => {
-                if (!g) {
-                  return <div key={i} className="gallery-img img-empty"></div>;
-                }
-                return (
-                  <div
-                    key={i}
-                    className="gallery-img"
-                    style={{
-                      backgroundImage: `url(${g})`,
-                      backgroundSize: 'cover',
-                    }}
-                  ></div>
-                );
-              })}
+              {!(island.gallery || []).length && (
+                <div>There is no gallery yet.</div>
+              )}
+              <Carousel showArrows={true} dynamicHeight swipeable>
+                {(island.gallery || []).map((g, i) => {
+                  return (
+                    <div key={i}>
+                      <img
+                        style={{
+                          height: '10%',
+                          maxHeight: '720px',
+                        }}
+                        alt={`gallery-` + i}
+                        src={g}
+                      />
+                    </div>
+                  );
+                })}
+              </Carousel>
             </div>
           </div>
-          {/* <div className="col-md-12 mb-5">
-            <h3>Free Items/DIY</h3>
-            <div>
-              {island.diy.map((d, i) => (
-                <div key={i}>{d}</div>
-              ))}
-            </div>
-          </div> */}
         </>
       ) : null}
     </div>
